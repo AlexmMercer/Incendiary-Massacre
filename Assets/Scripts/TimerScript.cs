0bd272feaf9cf7b8afaf12163eba4518284940c2
Alex Mercer <120363131+AlexmMercer@users.noreply.github.com>
2024-02-21 11:03:55 +0300

    C1

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    private float startTime;
+    private bool timerRunning = false;
+
+    void Start()
+    {
+        timerText.text = "0.000";
+        StartTimer();
+    }
+
+    void Update()
+    {
+        if (timerRunning)
+        {
+            float currentTime = Time.time - startTime;
+            string minutes = ((int)currentTime / 60).ToString("00");
+            string seconds = (currentTime % 60).ToString("00");
+            string milliseconds = ((currentTime * 1000) % 1000).ToString("000");
+            timerText.text = minutes + ":" + seconds + "." + milliseconds;
+        }
+    }
+
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        timerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        timerRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        timerText.text = "Time: 0.000";
+    }
+}
